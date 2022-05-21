7d901904bcaa7cafaa1266f214b76286af8e4069
lvxinbo <531852419@qq.com>
2022-05-21 20:34:56 +0800

    ✨ feat: 注册路由

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router, { setupRouter } from "./router"; // 路由
+const app = createApp(App);
+setupRouter(app); // 引入路由
+router.isReady().then(() => {
+  app.mount("#app");
+});
