a55377579c5d3d22e415ca9dd1928500cccef6ac
lvxinbo <531852419@qq.com>
2022-05-21 20:47:10 +0800

    ✨ feat: 引入element

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router, { setupRouter } from "./router"; // 路由
+import { setupElement } from "./plugins/Element";
+
 const app = createApp(App);
+setupElement(app);
 setupRouter(app); // 引入路由
 router.isReady().then(() => {
   app.mount("#app");
