a07a1d4f2cc3bb90fc67510ea6fdff84d6af0668
lvxinbo <531852419@qq.com>
2022-05-21 20:23:11 +0800

    🔧 build: 安装sass

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 const resolve = (dir: string) => path.join(__dirname, dir);
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
