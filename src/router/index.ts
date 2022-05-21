3a5c075de698c8eedb3b0a4b711b2adf6110bb95
lvxinbo <531852419@qq.com>
2022-05-21 20:33:31 +0800

    ✨ feat: 添加路由入口文件

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import type { App } from "vue";
+
+const routerHistory = createWebHistory();
+
+const router = createRouter({
+  history: routerHistory,
+  routes: [],
+});
+
+// 删除/重置路由
+export function resetRoute(): void {
+  router.getRoutes().forEach((route) => {
+    const { name } = route;
+    if (name) {
+      router.hasRoute(name) && router.removeRoute(name);
+    }
+  });
+}
+
+export function setupRouter(app: App<Element>) {
+  app.use(router);
+}
+export default router;
