3d54ad9c09d16e29936ec10fd5b2370ae6157668
lvxinbo <531852419@qq.com>
2022-05-21 21:54:48 +0800

    🐎 ci: 添加 prettierrc 配置

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  ignorePath: '.prettierignore' // 不使用prettier格式化的文件填写在项目的.prettierignore文件中
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-const resolve = (dir: string) => path.join(__dirname, dir);
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+const resolve = (dir: string) => path.join(__dirname, dir)
 
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),
+      resolvers: [ElementPlusResolver()]
+    })
   ],
   resolve: {
     alias: {
-      "@": resolve("src"),
-    },
-  },
-});
+      '@': resolve('src')
+    }
+  }
+})
