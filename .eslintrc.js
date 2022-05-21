d49ce24a7ca57a0674c3162a81d2858a833c02c1
lvxinbo <531852419@qq.com>
2022-05-21 21:51:15 +0800

    🐎 ci: 配置添加 prettier 和ts校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
   root: true,
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    parser: require.resolve("@typescript-eslint/parser"),
+    extraFileExtensions: [".vue"],
+  },
   env: {
     browser: true, // browser global variables
     es2021: true, // adds all ECMAScript 2021 globals and automatically sets the ecmaVersion parser option to 12.
     node: true,
   },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 12,
-  },
-  extends: ['plugin:vue/vue3-recommended', 'airbnb-base'],
-  rules: {
-    semi: ['error', 'never'],
-    quotes: ['error', 'single'],
-  },
-}
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "airbnb-base",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.vue"],
+    },
+  ],
+};
