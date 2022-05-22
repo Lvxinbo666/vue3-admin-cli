a21e92342132b81d8cf8d6c308e2b93af43312e5
lvxinbo <lvxinbo666@126.com>
2022-05-22 15:01:30 +0800

    🐎 ci: 关闭组件命名规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
     parser: require.resolve('@typescript-eslint/parser'),
     extraFileExtensions: ['.vue']
   },
+  rules: {
+    'vue/multi-word-component-names': 'off' // 关闭组件命名规则
+  },
   env: {
     browser: true, // browser global variables
     es2021: true, // adds all ECMAScript 2021 globals and automatically sets the ecmaVersion parser option to 12.
